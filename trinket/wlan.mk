46ee95f309e74a75611fe863446a0f46b4370ecc
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-31 16:53:10 +0530

    trinket: Disable wifi aware and RTT as per stock (ginkgo)

--- trinket/wlan.mk
+++ trinket/wlan.mk
@@ -7,16 +7,11 @@ WPA := wpa_cli
 PRODUCT_PACKAGES += wifilearner
 PRODUCT_PACKAGES += $(WPA)
 
-#Enable WIFI AWARE FEATURE
-WIFI_HIDL_FEATURE_AWARE := true
-
 PRODUCT_COPY_FILES += \
 	device/qcom/wlan/trinket/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
 	device/qcom/wlan/trinket/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 	device/qcom/wlan/trinket/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
 	device/qcom/wlan/trinket/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
-	frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
-	frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
         frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
 
 # WLAN specific aosp flag
